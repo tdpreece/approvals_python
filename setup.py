aad5c32ff951346c3198e952d075cd3b4f93f839
Tim Preece <tdpreece@gmail.com>
2016-10-16 13:16:42 +0100

    Add initial setup.py and tox.ini.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='approvals',
+    version='0.0.1',
+    py_modules='approvals',
+    url='https://github.com/tdpreece/approvals_python',
+    author='Tim Preece',
+    author_email='tdpreece@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
